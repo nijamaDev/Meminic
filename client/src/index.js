ac65b9ff08a47022edeadc4d56cc441fce923c8f
kiin The Dovah <kiinthedovah@gmail.com>
2022-01-30 15:50:39 -0500

    react env setted

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+console.log('process: ', domain);
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="meliwi.us.auth0.com"
-      clientId="Eyc69pxoAQhmOHWvRafhFBk5cdsCyyPB"
+      domain={domain}
+      clientId={clientId}
       redirectUri={window.location.origin}
     >
       <App />
     </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
