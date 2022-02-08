49233dd4663760e5941a247b7feadf7ba2f1cf74
Michelle <michelle.hernandez@correounivalle.edu.co>
2022-02-08 11:43:45 -0500

    inserting users partially working

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 ReactDOM.render(
+  /*
   <React.StrictMode>
     <Auth0Provider
       domain={domain}
@@ -14,6 +15,16 @@ ReactDOM.render(
     >
       <App />
     </Auth0Provider>
-  </React.StrictMode>,
+  </React.StrictMode>,*/
+  //Stricmode removed
+  
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+     ,
   document.getElementById("root")
 );
