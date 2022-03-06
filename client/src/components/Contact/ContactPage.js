2f01bd50b4babac0a2d1ee486d2c2dd544e34820
Melissa <melissa.gonzalez@correounivalle.edu.co>
2022-03-06 10:35:25 -0500

    verifications in form

--- client/src/components/Contact/ContactPage.js
+++ client/src/components/Contact/ContactPage.js
@@ -1,23 +1,20 @@
 import ContactForm from "../ContactForm/ContactForm";
-import map_img from "../../assets/map.png";
 import map_point_img from "../../assets/map-point.png";
 import call_img from "../../assets/call.png";
 import msg_img from "../../assets/msg.png";
 import compass_img from "../../assets/compass.png";
-import "./ContactPage.css"
+import "./ContactPage.css";
 import Card from "../Card/Card";
-
 
 const ContactPage = () => {
   return (
-      <>
+    <>
       <div className="contact__full">
         <div className="contact__container">
-          <h1 className="contact__title">
-          Contacto
-          </h1>
+          <h1 className="contact__title">Contacto</h1>
           <p className="contact__top__text">
-          Méminic ofrece servicios de calidad que incluyen el control de inventarios y contabilidad de tu tienda
+            Méminic ofrece servicios de calidad que incluyen el control de
+            inventarios y contabilidad de tu tienda
           </p>
         </div>
         <div className="contact__form__container">
@@ -28,19 +25,27 @@ const ContactPage = () => {
         </div>
         <div className="map__container">
           {/* <div className="map__img__point__container"> */}
-            <img className="map__point__img" src={map_point_img} alt="map point" />
+          <img
+            className="map__point__img"
+            src={map_point_img}
+            alt="map point"
+          />
           {/* </div> */}
           {/* <div className="map__img__container"> */}
-            {/* <img className="map__img" src={map_img} alt="map" /> */}
+          {/* <img className="map__img" src={map_img} alt="map" /> */}
           {/* </div> */}
         </div>
         <div className="info__container">
-          <Card img={call_img} title="TELÉFONO" content="2672076"/>
-          <Card img={msg_img} title="EMAIL" content="support@meminic.com"/>
-          <Card img={compass_img} title="DIRECCIÓN" content="2247 Lunetta Street, TX 76301"/>
+          <Card img={call_img} title="TELÉFONO" content="2672076" />
+          <Card img={msg_img} title="EMAIL" content="support@meminic.com" />
+          <Card
+            img={compass_img}
+            title="DIRECCIÓN"
+            content="2247 Lunetta Street, TX 76301"
+          />
         </div>
       </div>
-      </>
+    </>
   );
 };
 
